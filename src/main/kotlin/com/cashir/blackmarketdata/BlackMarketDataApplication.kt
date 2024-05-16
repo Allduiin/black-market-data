84e14d46c5492562304c084a9fbe1181676bbd8f
vprotsenko <vprotsenko@casgate.io>
2024-05-16 16:57:32 +0300

    Init commit

---
+++ src/main/kotlin/com/cashir/blackmarketdata/BlackMarketDataApplication.kt
@@ -0,0 +1,11 @@
+package com.cashir.blackmarketdata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlackMarketDataApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlackMarketDataApplication>(*args)
+}
---
+++ src/test/kotlin/com/cashir/blackmarketdata/BlackMarketDataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cashir.blackmarketdata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlackMarketDataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
