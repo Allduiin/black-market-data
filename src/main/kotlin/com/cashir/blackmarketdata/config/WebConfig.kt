3704b8b576fd9f2a02441123bf73647559aff71c
vprotsenko <vprotsenko@casgate.io>
2024-05-22 20:57:00 +0300

    Added russian names

--- src/main/kotlin/com/cashir/blackmarketdata/config/WebConfig.kt
+++ src/main/kotlin/com/cashir/blackmarketdata/config/WebConfig.kt
@@ -1,6 +1,5 @@
 package com.cashir.blackmarketdata.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
